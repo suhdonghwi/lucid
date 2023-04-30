d42ffe1892ea8df7e2391759104cd94a62ecc3a8
suhdonghwi <hwidongsuh@gmail.com>
2023-04-30 14:40:19 +0900

    blur on esc

--- src/components/EnhancedTextArea.tsx
+++ src/components/EnhancedTextArea.tsx
@@ -3,9 +3,16 @@ import React from "react";
 type EnhancedTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 function EnhancedTextArea({ ...props }: EnhancedTextAreaProps) {
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (e.key === "Escape") {
+      e.currentTarget.blur();
+    }
+  }
+
   return (
     <textarea
       {...props}
+      onKeyDown={handleKeyDown}
       autoCapitalize="off"
       autoComplete="off"
       autoCorrect="off"
