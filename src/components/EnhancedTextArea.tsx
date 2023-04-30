cad9527582f9254a9437c788a80ab3625160777a
suhdonghwi <hwidongsuh@gmail.com>
2023-04-30 14:37:43 +0900

    disable various checking on textarea

--- src/components/EnhancedTextArea.tsx
+++ src/components/EnhancedTextArea.tsx
@@ -3,7 +3,16 @@ import React from "react";
 type EnhancedTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 function EnhancedTextArea({ ...props }: EnhancedTextAreaProps) {
-  return <textarea {...props} />;
+  return (
+    <textarea
+      {...props}
+      autoCapitalize="off"
+      autoComplete="off"
+      autoCorrect="off"
+      spellCheck={false}
+      data-gramm={false}
+    />
+  );
 }
 
 export default EnhancedTextArea;
