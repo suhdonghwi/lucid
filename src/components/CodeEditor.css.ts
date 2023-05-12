c23a84a59959c99a50e267e727468361d5407c0c
suhdonghwi <hwidongsuh@gmail.com>
2023-05-12 14:11:09 +0900

    remove all legacy code editor and use CodeMirror instead

--- src/components/CodeEditor.css.ts
+++ src/components/CodeEditor.css.ts
@@ -1,7 +1,5 @@
 import { style } from "@vanilla-extract/css";
 
-const gutterWidth = "40px";
-
 export const rootContainer = style({
   width: "30rem",
   height: "30rem",
@@ -14,56 +12,3 @@ export const rootContainer = style({
   padding: "0.9rem 0.4rem",
   borderRadius: "10px",
 });
-
-export const editorContainer = style({
-  position: "relative",
-  minHeight: "100%",
-});
-
-const codeBase = style({
-  width: "100%",
-  height: "100%",
-
-  padding: 0,
-  margin: 0,
-  border: "none",
-  outline: "none",
-
-  wordBreak: "break-all",
-  whiteSpace: "pre-wrap",
-});
-
-export const codeTextArea = style([
-  codeBase,
-  {
-    resize: "none",
-    outline: "none",
-    WebkitTextFillColor: "transparent",
-
-    position: "absolute",
-    top: 0,
-    left: gutterWidth,
-    width: `calc(100% - ${gutterWidth})`,
-  },
-]);
-
-export const codeHighlight = style([
-  codeBase,
-  {
-    pointerEvents: "none",
-    display: "grid",
-    gridTemplateColumns: `${gutterWidth} 1fr`,
-  },
-]);
-
-export const lineNumber = style({
-  width: "100%",
-  textAlign: "right",
-  paddingRight: 12,
-
-  color: "#adb5bd",
-
-  wordBreak: "keep-all",
-});
-
-export const errorLine = style({ backgroundColor: "#ffe3e3" });
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,59 +1,10 @@
 import React from "react";
-
-import Highlight, { defaultProps } from "prism-react-renderer";
-import lightTheme from "prism-react-renderer/themes/github";
+import CodeMirror from "@uiw/react-codemirror";
+import { python } from "@codemirror/lang-python";
 
 import * as cls from "./CodeEditor.css";
-import CodeTextArea from "./CodeTextArea";
 
 import { RunError } from "../RunError";
-
-type CodeHighlightProps = {
-  code: string;
-  error: RunError | null;
-};
-
-function CodeHighlight({ code, error }: CodeHighlightProps) {
-  return (
-    <Highlight
-      {...defaultProps}
-      theme={lightTheme}
-      code={code}
-      language="python"
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={`${className} ${cls.codeHighlight}`}
-          style={{ ...style, backgroundColor: "transparent" }}
-        >
-          {tokens.map((line, i) => {
-            const lineNumber = i + 1;
-            const isErrorLine =
-              error && error.line <= lineNumber && lineNumber <= error.end_line;
-            const errorClassname = isErrorLine ? cls.errorLine : "";
-            return (
-              <React.Fragment key={i}>
-                <div className={`${errorClassname} ${cls.lineNumber}`}>
-                  {lineNumber}
-                </div>
-                <div
-                  {...getLineProps({
-                    line,
-                    className: errorClassname,
-                  })}
-                >
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token })} />
-                  ))}
-                </div>
-              </React.Fragment>
-            );
-          })}
-        </pre>
-      )}
-    </Highlight>
-  );
-}
 
 type CodeEditorProps = {
   code: string;
@@ -65,16 +16,12 @@ type CodeEditorProps = {
 function CodeEditor({ code, onCodeUpdate, error }: CodeEditorProps) {
   return (
     <div className={cls.rootContainer}>
-      <div className={cls.editorContainer}>
-        <CodeHighlight code={code} error={error} />
-        <CodeTextArea
-          className={cls.codeTextArea}
-          style={{ ...lightTheme.plain, backgroundColor: "transparent" }}
-          value={code}
-          onValueChange={onCodeUpdate}
-          tabSize={2}
-        />
-      </div>
+      <CodeMirror
+        value={code}
+        height="200px"
+        extensions={[python()]}
+        onChange={onCodeUpdate}
+      />
     </div>
   );
 }
