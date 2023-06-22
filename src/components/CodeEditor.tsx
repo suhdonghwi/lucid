8874600c562ef7a15f6ab38bfc4688bf04037271
suhdonghwi <hwidongsuh@gmail.com>
2023-06-22 17:24:44 +0900

    use useCodeMirror hook instead of component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import { EditorView } from "@codemirror/view";
 import { python } from "@codemirror/lang-python";
-import CodeMirror from "@uiw/react-codemirror";
+import { useCodeMirror } from "@uiw/react-codemirror";
 import { githubLightInit } from "@uiw/codemirror-theme-github";
 
 import * as cls from "./CodeEditor.css";
 
-const theme = EditorView.theme({
+const cssStyle = EditorView.theme({
   "&.cm-focused": {
     outline: "none",
+  },
+});
+
+const extensions = [cssStyle, python()];
+
+const theme = githubLightInit({
+  theme: "light",
+  settings: {
+    background: "transparent",
+    gutterBackground: "transparent",
+    fontFamily: "Fira Mono, monospace",
   },
 });
 
@@ -19,27 +30,29 @@ type CodeEditorProps = {
 };
 
 function CodeEditor({ code, onCodeUpdate }: CodeEditorProps) {
+  const editor = useRef<HTMLDivElement>(null);
+  const { setContainer } = useCodeMirror({
+    className: cls.editor,
+    value: code,
+    height: "100%",
+    theme,
+    extensions,
+    basicSetup: {
+      foldGutter: false,
+      highlightSelectionMatches: false,
+    },
+    onChange: onCodeUpdate,
+    container: editor.current,
+  });
+
+  useEffect(() => {
+    if (editor.current) 
+      setContainer(editor.current);
+  }, [editor.current]);
+
   return (
     <div className={cls.rootContainer}>
-      <CodeMirror
-        className={cls.editor}
-        value={code}
-        height="100%"
-        theme={githubLightInit({
-          theme: "light",
-          settings: {
-            background: "transparent",
-            gutterBackground: "transparent",
-            fontFamily: "Fira Mono, monospace",
-          },
-        })}
-        extensions={[theme, python()]}
-        basicSetup={{
-          foldGutter: false,
-          highlightSelectionMatches: false,
-        }}
-        onChange={onCodeUpdate}
-      />
+      <div ref={editor} />
     </div>
   );
 }
