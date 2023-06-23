35cbaa5073ebad0e1fdf7864b0a928c28d92f09c
suhdonghwi <hwidongsuh@gmail.com>
2023-06-23 13:43:58 +0900

    add eval highlight

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from "react";
 
-import { EditorView } from "@codemirror/view";
+import { useAnimate } from "framer-motion";
+
+import { EditorView, layer, RectangleMarker } from "@codemirror/view";
 import { python } from "@codemirror/lang-python";
 import { useCodeMirror } from "@uiw/react-codemirror";
 import { githubLightInit } from "@uiw/codemirror-theme-github";
@@ -8,13 +10,28 @@ import { githubLightInit } from "@uiw/codemirror-theme-github";
 import * as cls from "./CodeEditor.css";
 import { PosRange } from "../TrackData";
 
+const evalHighlightLayerClassName = "eval-highlight-layer";
+const evalHighlightClassName = "eval-highlight";
+
 const cssStyle = EditorView.theme({
   "&.cm-focused": {
     outline: "none",
   },
+  [`& .${evalHighlightClassName}`]: {
+    backgroundColor: "yellow",
+  },
 });
 
-const extensions = [cssStyle, python()];
+const evalHighlightLayer = layer({
+  class: evalHighlightLayerClassName,
+  above: false,
+  update: () => true,
+  markers: () => {
+    return [new RectangleMarker(evalHighlightClassName, 0, 0, 0, 0)];
+  },
+});
+
+const extensions = [cssStyle, python(), evalHighlightLayer];
 
 const theme = githubLightInit({
   theme: "light",
@@ -33,9 +50,14 @@ type CodeEditorProps = {
 
 function CodeEditor({ code, onCodeUpdate, highlight }: CodeEditorProps) {
   const editor = useRef<HTMLDivElement>(null);
-  const { setContainer, state, view } = useCodeMirror({
+  const { setContainer, view } = useCodeMirror({
     className: cls.editor,
+
+    // Not actually a 2-way binding
+    // But view dispatch does not occur if the value is same with view's internal state
+    // Reference: https://github.com/uiwjs/react-codemirror/blob/8a14a69d5bafdc6abdb14a90302031594771c5a3/core/src/useCodeMirror.ts#L160-L167
     value: code,
+
     height: "100%",
     theme,
     extensions,
@@ -47,20 +69,36 @@ function CodeEditor({ code, onCodeUpdate, highlight }: CodeEditorProps) {
     container: editor.current,
   });
 
+  const [scope, animate] = useAnimate();
+
   useEffect(() => {
     if (editor.current) setContainer(editor.current);
   }, [editor.current]);
 
   useEffect(() => {
-    if (!highlight || !state) return;
+    if (!(highlight && view)) return;
 
-    const pos1 = state.doc.line(highlight.line).from + highlight.col;
-    const pos2 = state.doc.line(highlight.end_line).from + highlight.end_col;
-    console.log(pos1, pos2);
+    const startPos = view.state.doc.line(highlight.line).from + highlight.col;
+    const endPos =
+      view.state.doc.line(highlight.end_line).from + highlight.end_col;
+
+    const startCoords = view.coordsAtPos(startPos);
+    const endCoords = view.coordsAtPos(endPos);
+
+    if (startCoords && endCoords && editor.current) {
+      const editorCoords = editor.current.getBoundingClientRect();
+
+      animate(`.${evalHighlightClassName}`, {
+        x: startCoords.left - editorCoords.left,
+        y: startCoords.top - editorCoords.top,
+        width: endCoords.left - startCoords.left,
+        height: startCoords.bottom - startCoords.top,
+      });
+    }
   }, [highlight]);
 
   return (
-    <div className={cls.rootContainer}>
+    <div ref={scope} className={cls.rootContainer}>
       <div ref={editor} />
     </div>
   );
