401de22e9336bca4a26e6db6c212669d5a106806
suhdonghwi <hwidongsuh@gmail.com>
2023-06-23 14:10:49 +0900

    disable eslint react-in-jsx-scope rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,4 +16,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { container } from "./App.css";
 
 import CodeRunner from "./components/CodeRunner";
