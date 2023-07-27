0d565b8ea136bd59d04471cd0fa5663cdaec8ae1
suhdonghwi <hwidongsuh@gmail.com>
2023-07-27 16:57:51 +0900

    make tracker add functions (truly) static

--- src/setup/tracker_attacher.py
+++ src/setup/tracker_attacher.py
@@ -3,51 +3,54 @@ from typing import TypeVar
 
 import tracker_identifier as IDENT
 
-def add_expr_tracker(node: ast.expr) -> ast.Call:
-    index_node = ast.Constant(value=node._index)
-
-    before_call = ast.Call(
-        func=ast.Name(id=IDENT.TRACKER_BEFORE_EXPR, ctx=ast.Load()),
-        args=[index_node],
-        keywords=[],
-    )
-
-    after_call = ast.Call(
-        func=ast.Name(id=IDENT.TRACKER_AFTER_EXPR, ctx=ast.Load()),
-        args=[before_call, node],
-        keywords=[],
-    )
-
-    return after_call
-
-def add_stmt_tracker(node: ast.stmt) -> ast.With:
-    index_node = ast.Constant(value=node._index)
-
-    tracker_call = ast.Call(
-        func=ast.Name(id=IDENT.TRACKER_STMT, ctx=ast.Load()),
-        args=[index_node],
-        keywords=[],
-    )
-
-    return ast.With(items=[ast.withitem(context_expr=tracker_call)], body=[node])
-
-def add_frame_tracker(index: int, body: list[ast.stmt]) -> list[ast.stmt]:
-    index_node = ast.Constant(value=index)
-
-    tracker_call = ast.Call(
-        func=ast.Name(id=IDENT.TRACKER_FRAME, ctx=ast.Load()),
-        args=[index_node],
-        keywords=[],
-    )
-    return [ast.With(items=[ast.withitem(context_expr=tracker_call)], body=body)]
-
 class TrackerAttacher(ast.NodeTransformer):
-    NodeType = TypeVar("Node", bound=ast.AST)
+    @staticmethod
+    def add_expr_tracker(node: ast.expr) -> ast.Call:
+        index_node = ast.Constant(value=node._index)
+
+        before_call = ast.Call(
+            func=ast.Name(id=IDENT.TRACKER_BEFORE_EXPR, ctx=ast.Load()),
+            args=[index_node],
+            keywords=[],
+        )
+
+        after_call = ast.Call(
+            func=ast.Name(id=IDENT.TRACKER_AFTER_EXPR, ctx=ast.Load()),
+            args=[before_call, node],
+            keywords=[],
+        )
+
+        return after_call
+
+    @staticmethod
+    def add_stmt_tracker(node: ast.stmt) -> ast.With:
+        index_node = ast.Constant(value=node._index)
+
+        tracker_call = ast.Call(
+            func=ast.Name(id=IDENT.TRACKER_STMT, ctx=ast.Load()),
+            args=[index_node],
+            keywords=[],
+        )
+
+        return ast.With(items=[ast.withitem(context_expr=tracker_call)], body=[node])
+
+    @staticmethod
+    def add_frame_tracker(index: int, body: list[ast.stmt]) -> list[ast.stmt]:
+        index_node = ast.Constant(value=index)
+
+        tracker_call = ast.Call(
+            func=ast.Name(id=IDENT.TRACKER_FRAME, ctx=ast.Load()),
+            args=[index_node],
+            keywords=[],
+        )
+        return [ast.With(items=[ast.withitem(context_expr=tracker_call)], body=body)]
+
+    NodeType = TypeVar("NodeType", bound=ast.AST)
     def visit(self, node: NodeType) -> NodeType:
         match node:
             case ast.Module():
                 node.body = list(map(self.visit, node.body))
-                node.body = add_frame_tracker(node._index, node.body)
+                node.body = self.add_frame_tracker(node._index, node.body)
                 return node
 
             case ast.Name(ctx=ast.Del()) | ast.Name(ctx=ast.Store()):
@@ -60,16 +63,16 @@ class TrackerAttacher(ast.NodeTransformer):
 
             case ast.expr():
                 self.generic_visit(node)
-                return add_expr_tracker(node)
+                return self.add_expr_tracker(node)
 
             case ast.FunctionDef():
                 node.body = list(map(self.visit, node.body))
-                node.body = add_frame_tracker(node._index, node.body)
-                return add_stmt_tracker(node)
+                node.body = self.add_frame_tracker(node._index, node.body)
+                return self.add_stmt_tracker(node)
 
             case ast.stmt():
                 self.generic_visit(node)
-                return add_stmt_tracker(node)
+                return self.add_stmt_tracker(node)
 
             case _:
                 self.generic_visit(node)
