f1fb242be8a07960825a663aa158cba17b6c99a8
Suh Donghwi <hwidongsuh@gmail.com>
2023-10-07 16:25:33 +0900

    add PythonCallbacks.ts

---
+++ src/python/PythonCallbacks.ts
@@ -0,0 +1,41 @@
+import { SyncExtras } from "comsync";
+
+import type { PyProxy } from "pyodide/ffi";
+
+import { EvalEvent, evalEventSchema } from "@/schemas/EvalEvent";
+import { FrameEvent, frameEventSchema } from "@/schemas/FrameEvent";
+
+export type PythonCallbacks = {
+  onStmtEnter: (event: EvalEvent) => void;
+  onStmtExit: (event: EvalEvent) => void;
+
+  onFrameEnter: (event: FrameEvent) => void;
+  onFrameExit: (event: FrameEvent) => void;
+};
+
+export const makePythonCallbacks = (
+  extras: SyncExtras,
+  callbacks: PythonCallbacks
+) => ({
+  stmt_enter: (maybeEvalEvent: PyProxy) => {
+    const event = evalEventSchema.parse(maybeEvalEvent);
+    callbacks.onStmtEnter(event);
+  },
+
+  stmt_exit: (maybeEvalEvent: PyProxy) => {
+    extras.readMessage();
+
+    const event = evalEventSchema.parse(maybeEvalEvent);
+    callbacks.onStmtExit(event);
+  },
+
+  frame_enter: (maybeFrameEvent: PyProxy) => {
+    const frameEvent = frameEventSchema.parse(maybeFrameEvent);
+    callbacks.onFrameEnter(frameEvent);
+  },
+
+  frame_exit: (maybeFrameEvent: PyProxy) => {
+    const frameEvent = frameEventSchema.parse(maybeFrameEvent);
+    callbacks.onFrameExit(frameEvent);
+  },
+});
--- src/python/worker.ts
+++ src/python/worker.ts
@@ -1,47 +1,12 @@
-import type { PyProxy } from "pyodide/ffi";
-
 import * as Comlink from "comlink";
-import { syncExpose, SyncExtras } from "comsync";
+import { syncExpose } from "comsync";
 
 import { initializePyodide } from "./initialize";
 
 import { ExecError, execErrorSchema } from "@/schemas/ExecError";
-import { EvalEvent, evalEventSchema } from "@/schemas/EvalEvent";
-import { FrameEvent, frameEventSchema } from "@/schemas/FrameEvent";
+import { PythonCallbacks, makePythonCallbacks } from "./PythonCallbacks";
 
 const pyodidePromise = initializePyodide();
-
-export type Callbacks = {
-  onStmtEnter: (event: EvalEvent) => void;
-  onStmtExit: (event: EvalEvent) => void;
-
-  onFrameEnter: (event: FrameEvent) => void;
-  onFrameExit: (event: FrameEvent) => void;
-};
-
-const makeCallbacksForPython = (extras: SyncExtras, callbacks: Callbacks) => ({
-  stmt_enter: (maybeEvalEvent: PyProxy) => {
-    const event = evalEventSchema.parse(maybeEvalEvent);
-    callbacks.onStmtEnter(event);
-  },
-
-  stmt_exit: (maybeEvalEvent: PyProxy) => {
-    extras.readMessage();
-
-    const event = evalEventSchema.parse(maybeEvalEvent);
-    callbacks.onStmtExit(event);
-  },
-
-  frame_enter: (maybeFrameEvent: PyProxy) => {
-    const frameEvent = frameEventSchema.parse(maybeFrameEvent);
-    callbacks.onFrameEnter(frameEvent);
-  },
-
-  frame_exit: (maybeFrameEvent: PyProxy) => {
-    const frameEvent = frameEventSchema.parse(maybeFrameEvent);
-    callbacks.onFrameExit(frameEvent);
-  },
-});
 
 export type RunPythonResult =
   | { type: "success" }
@@ -50,21 +15,21 @@ export type RunPythonResult =
 const api = {
   runPython: syncExpose(
     async (
-      extras,
+      syncExtras,
       interruptBuffer: Uint8Array,
       code: string,
-      callbacks: Callbacks
+      callbacks: PythonCallbacks
     ): Promise<RunPythonResult> => {
       const pyodide = await pyodidePromise;
       pyodide.setInterruptBuffer(interruptBuffer);
 
-      const callbacksForPython = makeCallbacksForPython(extras, callbacks);
+      const pythonCallbacks = makePythonCallbacks(syncExtras, callbacks);
 
-      pyodide.registerJsModule("js_callbacks", {});
-      const jsCallbacksModule = pyodide.pyimport("js_callbacks");
+      pyodide.registerJsModule("callbacks", {});
+      const callbacksModule = pyodide.pyimport("callbacks");
 
-      for (const [name, func] of Object.entries(callbacksForPython)) {
-        jsCallbacksModule[name] = func;
+      for (const [name, func] of Object.entries(pythonCallbacks)) {
+        callbacksModule[name] = func;
       }
 
       const fullCode = `from runner import run\nrun(${JSON.stringify(code)})`;
