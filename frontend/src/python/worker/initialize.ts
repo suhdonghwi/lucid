9ae828b8bfa943420bcf11058dddfc5df9f809e9
서동휘 <hwidongsuh@gmail.com>
2023-10-21 02:15:12 +0900

    import whl file using url loader

--- frontend/src/python/worker/initialize.ts
+++ frontend/src/python/worker/initialize.ts
@@ -3,13 +3,15 @@ import * as Comlink from "comlink";
 import type { PyodideInterface } from "pyodide";
 import { loadPyodide } from "pyodide";
 
+import pyodideBackendUrl from "./lucid_backend_pyodide.whl?url";
+
 async function initializePyodide(): Promise<PyodideInterface> {
   const indexURL = "https://cdn.jsdelivr.net/pyodide/v0.23.4/full/";
   const pyodide = await loadPyodide({ indexURL });
 
   pyodide.registerComlink(Comlink);
 
-  await pyodide.loadPackage("/lucid_backend_pyodide-0.0.1--none-any.whl");
+  await pyodide.loadPackage(pyodideBackendUrl);
   return pyodide;
 }
 
