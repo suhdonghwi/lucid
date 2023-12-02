e6f979cca295844a88db389d1ad13f1a4c9b7c6f
서동휘 <hwidongsuh@gmail.com>
2023-12-02 14:13:25 +0900

    move methods in indexed_ast out of the class

--- backend/pyodide/src/lucid_backend_pyodide/tracking/attacher/__init__.py
+++ backend/pyodide/src/lucid_backend_pyodide/tracking/attacher/__init__.py
@@ -5,60 +5,61 @@ from typing import TypeVar
 from . import identifier
 from ..callback import TrackerCallbacks
 from ..eval_context import EvalContext
-from .linearized_ast import LinearizedAST
+from .indexed_ast import IndexedAST, NodeIndex
+
+
+def create_index_node(index: NodeIndex) -> ast.Constant:
+    index_node = ast.Constant(value=index)
+    return index_node
+
+
+def create_call_node(func_name: str, args: list[ast.expr]) -> ast.Call:
+    return ast.Call(
+        func=ast.Name(id=func_name, ctx=ast.Load()),
+        args=args,
+        keywords=[],
+    )
+
+
+def add_expr_tracker(node: ast.expr, index: NodeIndex) -> ast.Call:
+    index_node = create_index_node(index)
+
+    before_call = create_call_node(identifier.TRACKER_BEFORE_EXPR, [index_node])
+    after_call = create_call_node(identifier.TRACKER_AFTER_EXPR, [before_call, node])
+
+    return after_call
+
+
+def add_stmt_tracker(node: ast.stmt, index: NodeIndex) -> ast.With:
+    index_node = create_index_node(index)
+    tracker_call = create_call_node(identifier.TRACKER_STMT, [index_node])
+
+    return ast.With(items=[ast.withitem(context_expr=tracker_call)], body=[node])
+
+
+def add_frame_tracker(
+    node: ast.FunctionDef | ast.Module, index: NodeIndex
+) -> list[ast.stmt]:
+    index_node = create_index_node(index)
+    tracker_call = create_call_node(identifier.TRACKER_FRAME, [index_node])
+
+    return [ast.With(items=[ast.withitem(context_expr=tracker_call)], body=node.body)]
 
 
 class TrackerAttacher(ast.NodeTransformer):
     def __init__(self, raw_ast: ast.Module):
         self.raw_ast = raw_ast
-        self.linearized_ast = LinearizedAST(raw_ast)
-
-    def create_index_node(self, node: ast.AST) -> ast.Constant:
-        index = self.linearized_ast.index_of(node)
-        index_node = ast.Constant(value=index)
-        return index_node
-
-    @staticmethod
-    def create_call_node(func_name: str, args: list[ast.expr]) -> ast.Call:
-        return ast.Call(
-            func=ast.Name(id=func_name, ctx=ast.Load()),
-            args=args,
-            keywords=[],
-        )
-
-    def add_expr_tracker(self, node: ast.expr) -> ast.Call:
-        index_node = self.create_index_node(node)
-
-        before_call = self.create_call_node(
-            identifier.TRACKER_BEFORE_EXPR, [index_node]
-        )
-        after_call = self.create_call_node(
-            identifier.TRACKER_AFTER_EXPR, [before_call, node]
-        )
-
-        return after_call
-
-    def add_stmt_tracker(self, node: ast.stmt) -> ast.With:
-        index_node = self.create_index_node(node)
-        tracker_call = self.create_call_node(identifier.TRACKER_STMT, [index_node])
-
-        return ast.With(items=[ast.withitem(context_expr=tracker_call)], body=[node])
-
-    def add_frame_tracker(self, node: ast.FunctionDef | ast.Module) -> list[ast.stmt]:
-        index_node = self.create_index_node(node)
-        tracker_call = self.create_call_node(identifier.TRACKER_FRAME, [index_node])
-
-        return [
-            ast.With(items=[ast.withitem(context_expr=tracker_call)], body=node.body)
-        ]
+        self.indexed_ast = IndexedAST(raw_ast)
 
     Node = TypeVar("Node", bound=ast.AST)
 
     def visit(self, node: Node) -> Node:
+        index = self.indexed_ast.index_of(node)
+
         match node:
             case ast.Module():
                 node.body = list(map(self.visit, node.body))
-                node.body = self.add_frame_tracker(node)
+                node.body = add_frame_tracker(node, index)
                 return node
 
             case ast.Name(ctx=ast.Del()) | ast.Name(ctx=ast.Store()):
@@ -74,16 +75,16 @@ class TrackerAttacher(ast.NodeTransformer):
 
             case ast.expr():
                 self.generic_visit(node)
-                return self.add_expr_tracker(node)
+                return add_expr_tracker(node, index)
 
             case ast.FunctionDef():
                 node.body = list(map(self.visit, node.body))
-                node.body = self.add_frame_tracker(node)
-                return self.add_stmt_tracker(node)
+                node.body = add_frame_tracker(node, index)
+                return add_stmt_tracker(node, index)
 
             case ast.stmt():
                 self.generic_visit(node)
-                return self.add_stmt_tracker(node)
+                return add_stmt_tracker(node, index)
 
             case _:
                 self.generic_visit(node)
@@ -94,31 +95,31 @@ class TrackerAttacher(ast.NodeTransformer):
         return ast.fix_missing_locations(self.visit(self.raw_ast))
 
     def create_tracker_mappings(self, callbacks: TrackerCallbacks):
-        def tracker_before_expr(node_index: int):
+        def tracker_before_expr(index: NodeIndex):
             frame = sys._getframe(1)
-            node = self.linearized_ast[node_index]
+            node = self.indexed_ast.node_of(index)
             assert isinstance(node, ast.expr)
 
             callbacks.before_expr(frame, node)
-            return node_index
+            return index
 
-        def tracker_after_expr(node_index: int, value: object):
+        def tracker_after_expr(index: NodeIndex, value: object):
             frame = sys._getframe(1)
-            node = self.linearized_ast[node_index]
+            node = self.indexed_ast.node_of(index)
             assert isinstance(node, ast.expr)
 
             callbacks.after_expr(frame, node, value)
             return value
 
-        def tracker_stmt(node_index: int):
-            node = self.linearized_ast[node_index]
+        def tracker_stmt(index: NodeIndex):
+            node = self.indexed_ast.node_of(index)
             assert isinstance(node, ast.stmt)
 
             return EvalContext(node, callbacks.before_stmt, callbacks.after_stmt)
 
         # TODO: Handle lambda expression
-        def tracker_frame(node_index: int):
-            node = self.linearized_ast[node_index]
+        def tracker_frame(index: NodeIndex):
+            node = self.indexed_ast.node_of(index)
             assert isinstance(node, (ast.FunctionDef, ast.Lambda, ast.Module))
 
             return EvalContext(node, callbacks.before_frame, callbacks.after_frame)
--- backend/pyodide/src/lucid_backend_pyodide/tracking/loader.py
+++ backend/pyodide/src/lucid_backend_pyodide/tracking/loader.py
@@ -1,7 +1,11 @@
 from importlib.abc import Loader
 from importlib.machinery import ModuleSpec
 
+import ast
+
 from types import ModuleType
+
+from lucid_backend_pyodide.tracking.attacher import TrackerAttacher
 
 
 class TrackingLoader(Loader):
@@ -12,9 +16,14 @@ class TrackingLoader(Loader):
         return None  # use default module creation semantics
 
     def exec_module(self, module: ModuleType):
-        with open(self.filename) as f:
-            data = f.read()
+        with open(self.filename) as file:
+            source = file.read()
 
-        # manipulate data some way...
+        raw_ast = ast.parse(source, self.filename)
 
-        exec(data, vars(module))
+        attacher = TrackerAttacher(raw_ast)
+        tracked_ast = attacher.attach()
+
+        compiled_code = compile(tracked_ast, filename=self.filename, mode="exec")
+
+        exec(compiled_code, vars(module))
