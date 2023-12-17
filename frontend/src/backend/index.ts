024fa50c20bd9d5a66ea51ab7001496f936f2e9a
서동휘 <hwidongsuh@gmail.com>
2023-12-18 02:13:44 +0900

    add terminate functionality

--- frontend/src/backend/index.ts
+++ frontend/src/backend/index.ts
@@ -3,4 +3,16 @@ import * as Comlink from "comlink";
 import BackendWorker from "./worker?worker";
 import { BackendAPI } from "./worker";
 
-export const backendWorker = Comlink.wrap<BackendAPI>(new BackendWorker());
+let worker = new BackendWorker();
+let wrappedWorker = Comlink.wrap<BackendAPI>(worker);
+
+export function executeCode(code: string) {
+  return wrappedWorker.executeCode(code);
+}
+
+export function terminateWorker() {
+  worker.terminate();
+
+  worker = new BackendWorker();
+  wrappedWorker = Comlink.wrap<BackendAPI>(worker);
+}
--- frontend/src/components/ExecutionController/index.tsx
+++ frontend/src/components/ExecutionController/index.tsx
@@ -3,21 +3,26 @@ import { useState } from "react";
 import { CodeWindow } from "@/components/CodeWindow";
 import * as cls from "./index.css";
 
-import { backendWorker } from "@/backend";
+import { executeCode, terminateWorker } from "@/backend";
 
 const exampleCode = `console.log("hi");`;
 
 export function ExecutionController() {
   const [code, setCode] = useState(exampleCode);
 
-  async function runCode() {
-    backendWorker.executeCode(code);
+  function runCode() {
+    executeCode(code);
+  }
+
+  function terminate() {
+    terminateWorker();
   }
 
   return (
     <div className={cls.rootContainer}>
       <div className={cls.buttonContainer}>
         <input type="button" value="Run" onClick={runCode} />
+        <input type="button" value="Terminate" onClick={terminate} />
       </div>
 
       <div className={cls.windowContainer}>
