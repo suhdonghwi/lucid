44ce11082425cdffd795c6e0d12e1d3ccc961a3f
서동휘 <hwidongsuh@gmail.com>
2023-12-30 17:08:49 +0900

    shared module experiment

--- frontend/src/backend/executeWorker.ts
+++ frontend/src/backend/executeWorker.ts
@@ -5,17 +5,36 @@ const createCodeBlob = (input: string) =>
   new Blob([input], { type: "text/javascript" });
 
 const api = {
-  executeCode: (code: string) => {
+  executeCode: async (code: string) => {
     const instrumentedCode = instrument(code);
     console.log(instrumentedCode);
 
-    const codeBlob = createCodeBlob(instrumentedCode);
+    const sharedModuleCodeBlob = createCodeBlob(`
+      export const enterCount = [];
+      export const enter = () => {
+        enterCount.push(1);
+        console.log("enter");
+      }
+      export const leave = () => console.log("leave");
+    `);
+    const sharedModuleObjectURL = URL.createObjectURL(sharedModuleCodeBlob);
+
+    const codeBlob = createCodeBlob(
+      "import {enter,leave} from " +
+        JSON.stringify(sharedModuleObjectURL) +
+        ";\n" +
+        instrumentedCode,
+    );
     const objectURL = URL.createObjectURL(codeBlob);
-    import(
+    await import(
       /* @vite-ignore */
       objectURL
     );
     URL.revokeObjectURL(objectURL);
+
+    import(sharedModuleObjectURL).then((module) => {
+      console.log(module);
+    });
   },
 };
 
