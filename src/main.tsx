f192634e8198ae5776454b737ec0684f30fbfa1f
서동휘 <hwidongsuh@gmail.com>
2024-01-13 17:55:29 +0900

    setup solid

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
-console.log("hello!");
+/* @refresh reload */
+import { render } from "solid-js/web";
+
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+
+render(() => <App />, root!);
